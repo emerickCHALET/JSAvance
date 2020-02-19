89075c537cd0337c1ffdc443d58d7c1d54692647
Furradev <brian.lecarpentier@edu.itescia.fr>
2020-02-19 13:39:48 +0100

    test

--- tdi/src/firebase/StoreScore.js
+++ tdi/src/firebase/StoreScore.js
@@ -4,11 +4,43 @@ import firebase from './firebase';
 
 export default function StoreScore(name, number, turn){
 
-    // const scoreKey = this.scores.push().key;
-
     firebase.database().ref('scores/').push({
         name: name,
         number: number,
         turn: turn
     });
 }
+
+export function getScore(){
+
+    firebase.database().ref('scores/').on('value',  (snapshot) =>{
+        let items = snapshot.val();
+        let newState = [];
+        for (let item in items) {
+            newState.push({
+                name: items[item].name,
+                number: items[item].number,
+                turn: items[item].turn,
+            });
+        }
+
+        console.log(newState);
+        return newState;
+    })
+}
+
+/* export function getNumber(){
+
+    firebase.database().ref('scores/').on('child_added',  (snapshot) =>{
+        console.log(snapshot.val().number);
+        return snapshot.val().number;
+    })
+}
+
+export function getTurn(){
+
+    firebase.database().ref('scores/').on('child_added',  (snapshot) =>{
+        console.log(snapshot.val().turn);
+        return snapshot.val().turn;
+    })
+} */

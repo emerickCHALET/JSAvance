6965ba42b4b96c554dcfcf3fe4d09fdd1d0fabd1
EMERICKCHALET <emerick.chalet@edu.ietscia.fr>
2020-02-20 11:46:12 +0100

    change of design home and add rules

--- codingdojo/src/components/Home.js
+++ codingdojo/src/components/Home.js
@@ -21,7 +21,13 @@ class Home extends React.Component {
                     <input type="text"></input>
                     <button>Submit</button>
                 </form>
-
+                <h2>Rules of the game</h2>
+                <p>The rules of this game are simple.</p>
+                <p>During 30 sec , you will see many case in red an others in black. You need to remember where are the red cases.</p>
+                <p>After this time, your goal will be to click where the red case were. If you succeed, you won and will have a new board with a new game and you take 1 points.</p>
+                <p>If you loose, the game restart but your score will be added to the board of results and you will can see your classment.</p>
+                <p>!!!Don't forgot to enter your Name!!!</p>
+                <p>Good Luck.</p>
             </div>
         );
     }
--- codingdojo/src/components/Routing.js
+++ codingdojo/src/components/Routing.js
@@ -27,18 +27,18 @@ export default class Routing extends React.Component {
                     <nav>
                         <ul>
                             <li>
-                                <Link to="/">Home</Link>
+                                <Link to="/" class="hey">Home</Link>
                             </li>
                             <li>
-                                <Link to="/about">About</Link>
+                                <Link to="/about" class="hey">About</Link>
                             </li>
                             {this.test &&
                                 <li>
-                                    <Link to="/Board">Enter game</Link>
+                                    <Link to="/Board" class="hey">Enter game</Link>
                                 </li>
                             }
                             <li>
-                                <Link to="/Scoreboard">Scoreboard</Link>
+                                <Link to="/Scoreboard" class="hey">Scoreboard</Link>
                             </li>
                         </ul>
                     </nav>
